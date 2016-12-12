4c9e657dbfadfe8c9e81a654e17b629a2f553b50
qxg <58342423@qq.com>
2016-12-12 11:40:53 +0800

    2016.12.12

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<!doctype HTML>
+<html>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<link rel="stylesheet" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" href="./css/index.css">
+	<head>
+	</head>
+
+	<body>
+		
+	</body>
+
+	<body>
+		<div class="top-panel">
+			<div class="login">
+				<form action="" method="post">
+					<fieldset class="form-group">
+						<label for="user">账号:</label>
+						<input class="form-control" id="user" placeholder="请输入账号">
+					</fieldset>
+					<fieldset class="form-group">
+						<label for="pwd">密码:</label>
+						<input type="password" class="form-control" id="pwd" placeholder="请输入密码">
+					</fieldset>
+
+					<button type="submit" class="btn btn-outline-info">登陆</button>
+				</form>
+			</div>
+		</div>
+
+		<script src="./js/jquery-3.1.1.min.js"></script>
+		<script src="./js/bootstrap.min.js"></script>
+	</body>
+</html>
